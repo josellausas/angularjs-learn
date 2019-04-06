b594bd8f91af127870490224ddb3afa0fe40874b
zunware <jose@josellausas.com>
2019-04-05 21:24:30 -0600

    Added Enter key press callback

--- src/app.js
+++ src/app.js
@@ -1,6 +1,24 @@
 var myApp = angular.module('myApp', []);
 
-// Delcare a controller
+myApp.directive('onEnterKeyPressed', function() {
+  return function ($scope, element, attrs) {
+    element.bind("keydown press", function(event) {
+      if(event.which === 13) {
+        // ENTER
+        $scope.$apply(function(){
+          $scope.$eval(attrs.onEnterKeyPressed);
+        });
+        event.preventDefault();
+      }
+    });
+  }
+});
+
+
 myApp.controller('mainController', function($scope) {
-  $scope.name = 'Carnalito Angular'
+  $scope.name = 'Carnalito Angular';
+  $scope.updateValue = function() {
+    alert('Updated!');
+    $scope.name = 'Another one bites the dust';
+  };
 });
